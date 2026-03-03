831b7f4889a434511d37d465a8d3e342a6337451
surajit-13-sutradhar <surajitsutradhardes@gmail.com>
2026-03-03 10:28:02 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops_us_visa",
+    version="0.0.0",
+    author="surajit",
+    author_email="surajitsutradhardes@gmail.com",
+    packages=find_packages()
+)
